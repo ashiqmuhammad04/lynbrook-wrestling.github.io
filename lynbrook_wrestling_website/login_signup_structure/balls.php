b4e68046cb25e9e7bf8f92f44e89dde07c76eced
Muhammad Ashiq <ashiqmuhammad04@gmail.com>
2021-07-30 21:33:34 -0700

    first push B)

---
+++ lynbrook_wrestling_website/login_signup_structure/balls.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php 
+            #$name = $_POST("uname");
+            #$pass = $_POST("psw");
+            $name == "LynCoach";
+            $pass == "Vikings";
+
+            if ($name == "LynCoach" and $pass == "Vikings") { 
+                fopen("html/coach_login_handler.html", "r");
+            }
+        ?>
+    </body>
+<html>
+    
